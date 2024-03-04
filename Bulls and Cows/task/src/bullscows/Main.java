8bd4058fdcc63c849f87191bca40bd2e4fe3e666
rasidagac <agacrasid@outlook.com>
2024-03-04 07:28:45 +0300

    Stage 2 is completed

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,17 +1,37 @@
 package bullscows;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                The secret code is prepared: ****.
+        Scanner scanner = new Scanner(System.in);
+        String secretCode = String.valueOf(3536);
+        String guess = scanner.nextLine();
 
-                Turn 1. Answer:
-                1234
-                Grade: None.
+        do {
+            int cows = 0;
+            int bulls = 0;
+            for (int i = 0; i < secretCode.length(); i++) {
+                char digit = guess.charAt(i);
+                if (digit == secretCode.charAt(i)) {
+                    bulls++;
+                } else if (secretCode.indexOf(digit) != -1) {
+                    cows++;
+                }
+            }
+            if (bulls == 0 && cows == 0) {
+                System.out.println("None. The secret code is " + secretCode);
+            } else if (bulls == 0) {
+                System.out.println("Grade: " + cows + " cow(s). The secret code is " + secretCode);
+            } else if (cows == 0) {
+                System.out.println("Grade: " + bulls + " bull(s). The secret code is " + secretCode);
+            } else {
+                System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretCode);
+            }
+            guess = scanner.nextLine();
+        } while (!Objects.equals(guess, secretCode));
 
-                Turn 2. Answer:
-                9876
-                Grade: 4 bulls.
-                Congrats! The secret code is 9876.""");
     }
+
 }
