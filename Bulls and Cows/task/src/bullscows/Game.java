3612b25e97e3f1d4d4651f442ff922ba3aa8d6ed
rasidagac <agacrasid@outlook.com>
2024-03-04 07:30:41 +0300

    Stage 7 is completed

--- Bulls and Cows/task/src/bullscows/Game.java
+++ Bulls and Cows/task/src/bullscows/Game.java
@@ -14,24 +14,30 @@ public class Game {
     public void start() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input the length of the secret code:");
-        int secretCodeLength = scanner.nextInt();
-        System.out.println("Input the number of possible symbols in the code:");
-        int possibleSymbols = scanner.nextInt();
+        String secretCodeLength = scanner.nextLine();
 
-        checkSecretCodeLength(secretCodeLength, possibleSymbols);
+        if (!secretCodeLength.matches("\\d+") || Integer.parseInt(secretCodeLength) < 1 || Integer.parseInt(secretCodeLength) > SecretCodeGenerator.MAX_SECRET_CODE_LENGTH) {
+            System.out.println("Error: \"" + secretCodeLength + "\" isn't a valid number.");
+        } else {
+            System.out.println("Input the number of possible symbols in the code:");
+            int possibleSymbols = scanner.nextInt();
+            checkSecretCode(Integer.parseInt(secretCodeLength), possibleSymbols);
+        }
     }
 
-    private void checkSecretCodeLength(int length, int possibleSymbols) {
-        if (length > possibleSymbols) {
-            System.out.println("Error: it's not possible to generate a code with a length of " + length + " with " + possibleSymbols + " unique symbols.");
-        } else if (length > SecretCodeGenerator.MAX_SECRET_CODE_LENGTH) {
-            System.out.println("Error: can't generate a secret number with a length of " + length + " because there aren't enough unique digits.");
+    private void checkSecretCode(int length, int possibleSymbols) {
+        if (possibleSymbols > SecretCodeGenerator.MAX_SECRET_CODE_LENGTH) {
+            System.out.println("Error: maximum number of possible symbols in the code is " + SecretCodeGenerator.MAX_SECRET_CODE_LENGTH + " (0-9, a-z).");
         } else {
-            secretCode = secretCodeGenerator.generateSecretCode(length, possibleSymbols);
-            String stringWillPrinted = possibleSymbols < 11 ? "0-" + (possibleSymbols - 1) : "0-9, a-" + SecretCodeGenerator.possibleSymbolsString.charAt(possibleSymbols - 1);
-            System.out.println("The secret is prepared: " + "*".repeat(length) + " " + "(" + stringWillPrinted + ")");
-            System.out.println("Okay, let's start a game!");
-            guessSecretCode();
+            if (length > possibleSymbols) {
+                System.out.println("Error: it's not possible to generate a code with a length of " + length + " with " + possibleSymbols + " unique symbols.");
+            } else {
+                secretCode = secretCodeGenerator.generateSecretCode(length, possibleSymbols);
+                String stringWillPrinted = possibleSymbols < 11 ? "0-" + (possibleSymbols - 1) : "0-9, a-" + SecretCodeGenerator.possibleSymbolsString.charAt(possibleSymbols - 1);
+                System.out.println("The secret is prepared: " + "*".repeat(length) + " " + "(" + stringWillPrinted + ")");
+                System.out.println("Okay, let's start a game!");
+                guessSecretCode();
+            }
         }
     }
 
--- Bulls and Cows/task/src/bullscows/SecretCodeGenerator.java
+++ Bulls and Cows/task/src/bullscows/SecretCodeGenerator.java
@@ -2,7 +2,7 @@ package bullscows;
 
 public class SecretCodeGenerator implements ISecretCodeGenerator {
     static final String possibleSymbolsString = "0123456789abcdefghijklmnopqrstuvwxyz";
-    static final int MAX_SECRET_CODE_LENGTH = 10;
+    static final int MAX_SECRET_CODE_LENGTH = 36;
 
     @Override
     public String generateSecretCode(int length, int possibleSymbols) {
